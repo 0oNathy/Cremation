09d9ff5c01c84ed892d0452de882c6eaa8c0834b
raven <120398403+0oNathy@users.noreply.github.com>
2023-02-05 12:14:29 +0800

    Update and rename CremationLoader.luau to CremationLoader.lua

---
+++ CremationLoader.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 2753915549 then
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/0oNathy/Cremation/main/CremationBloxFruitsLoader.luau"))()
+end
+if game.PlaceId == 4442272183 then
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/0oNathy/Cremation/main/CremationBloxFruitsLoader.luau"))()
+elseif game.PlaceId == 7449423635 then
+	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/0oNathy/Cremation/main/CremationBloxFruitsLoader.luau"))()
+else
+    game.Players.LocalPlayer:Kick("Game is not supported. ( 507 )")
+end
