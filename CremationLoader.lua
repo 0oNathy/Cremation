a5e7a9799ba9b70995d86c543890f94dacf8cb68
raven <120398403+0oNathy@users.noreply.github.com>
2023-02-07 17:45:39 +0800

    Update CremationLoader.lua

--- CremationLoader.lua
+++ CremationLoader.lua
@@ -7,6 +7,6 @@ if game.PlaceId == 4442272183 then
 elseif game.PlaceId == 7449423635 then
 	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/0oNathy/Cremation/main/CremationBloxFruitsLoader.luau"))()
 else
-    game.Players.LocalPlayer:Kick("Game is not supported. ( 507 )")
+    game.Players.LocalPlayer:Kick("Game is not supported.")
 end
 -- //
